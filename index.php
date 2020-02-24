9e58c95e0ff86047c4e34333f199b437ff339fd2
rmurdoch <oatsrobert81@gmail.com>
2020-02-24 10:59:39 +0200

    Exploration: started getting db connected to code

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-echo "Hello, world this is my first Heroku app"
+$db  = new SQLite3('test.sqlite');
+
+//  $input = readline("Enter your name :");
+$outputFromDb = $db->query('SELECT * from users');
+// echo $input;
+$users = array();
+$i = 0;
+while ($res = $outputFromDb->fetchArray(SQLITE3_ASSOC)) {
+    $users[$i]['name'] = $res['name'];
+    $i++;
+}
+for ($j=0; $j < count($users); $j++) { 
+    echo ($users[$j]['name']);
+}
 ?>
